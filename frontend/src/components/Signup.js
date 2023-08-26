e9c78a23ab7b33a3dee6f6baf2d74d5383ff21b3
Harshakabari <harshakabariedu@gmail.com>
2023-08-26 16:02:10 +0530

    sdhd

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,87 +1,92 @@
-import React from 'react'
-import { useState } from 'react';
+// src/components/SignupForm.js
+import React, { useState } from "react";
 
-const signup = () => {
-  
-    const [username, setUsername] = useState(null);
-    const [email, setEmail] = useState(null);
-    const [password,setPassword] = useState(null);
-    const [mobile,setMobile] = useState(null);
+function SignupForm() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    mobile: "",
+  });
 
-    const handleInputChange = (e) => {
-        const {id , value} = e.target;
-        if(id === "username"){
-            setUsername(value);
-        }
-        if(id === "email"){
-            setEmail(value);
-        }
-        if(id === "password"){
-            setPassword(value);
-        }
-        if(id === "mobile"){
-            setMobile(value);
-        }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try{
+        const responce = await fetch("http://localhost:5000/signup",{
+            method:  'post',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(formData),
+
+        });
+        const data =await responce.json();
+        console.log(data);
+    }catch(error){
+        console.log('error while register');
     }
+  };
 
-    const handleSubmit  = () => {
-        e.preventDefault();
-
-        const credentials = {
-          username,
-          password,
-          email,
-          mobile
-         };
-
-         try {
-            const response = fetch('http://localhost:5000/signup', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(credentials),
-            });
-
-            setEmail('');
-            setMobile('');
-            setPassword('');
-            setUsername('');
-        }catch (error) {
-            console.error('Error during signup:', error);
-          }
-        
-    };
-
-    return(
-        <div className="form">
-            <div className="form-body">
-                <div className="username">
-                    <label className="form__label" for="firstName">user Name </label>
-                    <input className="form__input" type="text" value={username} onChange = {(e) => handleInputChange(e)} id="username" placeholder="user Name"/>
-                </div>
-                
-                <div className="email">
-                    <label className="form__label" for="email">Email </label>
-                    <input  type="email" id="email" className="form__input" value={email} onChange = {(e) => handleInputChange(e)} placeholder="Email"/>
-                </div>
-                <div className="password">
-                    <label className="form__label" for="password">Password </label>
-                    <input className="form__input" type="password"  id="password" value={password} onChange = {(e) => handleInputChange(e)} placeholder="Password"/>
-                </div>
-                <div className="mobile">
-                    <label className="form__label" for="mobile">mobile no </label>
-                    <input className="form__input" type="number" id="mobile" value={mobile} onChange = {(e) => handleInputChange(e)} placeholder="mobile"/>
-                </div>
-            </div>
-            <div class="footer">
-                <button onClick={()=>handleSubmit()} type="submit" class="btn">Register</button>
-            </div>
+  return (
+    <div>
+      <h2>Signup Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
         </div>
-       
-    )    
-  
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="mobile">Mobile:</label>
+          <input
+            type="tel"
+            id="mobile"
+            name="mobile"
+            value={formData.mobile}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
-export default signup;
+export default SignupForm;
