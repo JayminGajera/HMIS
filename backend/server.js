3f7cf833d5610bfe8e40b367c8e755e75f456f9c
POOJANGHETIYA <ghetiyapoojan@gmail.com>
2023-08-26 16:15:22 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,49 +1,40 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const {MONGODURL} = require('./config');
-const cors = require('cors');
-
-// use cors for all requests
-app.use(cors());
+const cors = require('cors'); // Import the cors middleware
+const { MONGODURL } = require('./config');
 
 const app = express();
 const port = 5000;
 
+// Use the cors middleware to allow all origins
+app.use(cors());
+
 app.use(express.json());
-// Connect to MongoDB
-mongoose.connect(MONGODURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((error) => {
-  console.error('Failed to connect to MongoDB:', error);
-});
 
-// Use body-parser to parse incoming requests
-// app.use(bodyParser.urlencoded({ extended: true }));
+mongoose
+  .connect(MONGODURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
 
-
-// Import routes
 const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
 const reset = require('./routes/reset');
 const verifyEmail = require('./routes/verifyregister');
 const resetEmail = require('./routes/verifyreset');
 
-// use routes
 app.use(loginRoute);
 app.use(registerRoute);
 app.use(reset);
 app.use(verifyEmail);
 app.use(resetEmail);
 
-
-
-// Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
