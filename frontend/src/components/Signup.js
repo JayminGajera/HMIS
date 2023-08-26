6357db33f7927caad64dca0626d7cf498b706a63
Meetpaneliya <meetpaneliya032@gmail.com>
2023-08-26 16:09:19 +0530

    commit1

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -36,7 +36,7 @@ const signup = () => {
          };
 
          try {
-            const response =  await fetch('http://localhost:5000/signup', {
+            const response =  await fetch('http://localhost:5000/register', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
