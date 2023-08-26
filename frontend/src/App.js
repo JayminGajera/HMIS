96cb83d8cc9c3c9d386a1708e22cb9e3ee8e6b6a
JayminGajera <jaymingajera2332@gmail.com>
2023-08-26 14:11:43 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-
+import Navbar from './components/Navbar'
+import './components/navbar.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
         
     </div>
   );
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
