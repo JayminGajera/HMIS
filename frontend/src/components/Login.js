2c48fd1fa6f854eff7d2d2ce5ce422231610e0da
POOJANGHETIYA <ghetiyapoojan@gmail.com>
2023-08-26 15:47:39 +0530

    login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const login = () => {
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Create a credentials object with email and password
+    const credentials = {
+       email,
+       password,
+    };
+
+    try {
+      // Send a POST request to /api/login
+      const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        // Set the credentials in the request body
+        body: JSON.stringify(credentials),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default login
+
+export default Login;
