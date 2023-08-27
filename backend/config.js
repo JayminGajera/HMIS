bd23f6f1fc55a7aa5ace25d778371b41a8c5be1d
JayminGajera <jaymingajera2332@gmail.com>
2023-08-27 12:13:51 +0530

    config deleted

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,3 @@
 module.exports = {
-     email: 'intern@pruthatek.com', // Replace the mail id
-     password: 'lnzdqtdngorinwzi', // Replace password for less secure apps
-     secret_key: 'sage1234',   // Secret key for generate jwt token
-     MONGODURL: 'mongodb+srv://poojan:poojan@hmis.bp8ppsb.mongodb.net/doctor'
+     
 };
